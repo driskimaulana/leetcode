f7d3b02781001ee26ddf61757c12c916c5e485e6
D'Riski Maulana <68141563+driskimaulana@users.noreply.github.com>
2023-07-24 14:41:50 +0700

    Time: 29 ms (30.26%) | Memory: 4.5 MB (29.53%) - LeetSync

--- 9-palindrome-number/palindrome-number.go
+++ 9-palindrome-number/palindrome-number.go
@@ -1,5 +1,9 @@
 func isPalindrome(x int) bool {
   xString := strconv.Itoa(x)
+	if(len(xString) < 2) {
+		return true
+	} 
+
 	xRunes := []rune(xString)
 	j := len(xRunes) - 1
 	for i := 0; i < len(xRunes); i++ {
